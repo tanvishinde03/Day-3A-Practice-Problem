4b9b30e5e9e6b7ad91f3b09dc803a52cb41d1c32
Tanvishinde03 <shindetanvi0203@gmail.com>
2023-04-13 15:43:11 +0530

    [Tanvi] Add : UC1 - program with object and class concept

---
+++ Day-3A-Practice-Problem/ClassNObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3A_Practice_Problem
+{
+    public class ClassNObject   // Define a class called "Person"
+    {
+        public string Name;
+        public int Age;
+                                    // Class methods
+        public void SayHello()
+        {
+            Console.WriteLine("Hello, my name is " + Name + " and I am " + Age + " years old.");
+        }
+    }
+
+}
+
---
+++ Day-3A-Practice-Problem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3A_Practice_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassNObject person1 = new ClassNObject(); // Creating object
+           ClassNObject  person2 = new ClassNObject();
+
+            person1.Name = "Tanvi";
+            person1.Age = 22;
+
+            person2.Name = "Aarti";
+            person2.Age = 22;
+
+            person1.SayHello();
+            person2.SayHello();
+            Console.ReadLine();
+        }
+    }
+}
